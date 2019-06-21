244c636b1bbe058786f4ea215ccc6cdb1cd78d0c
Dryunkaaa <the_quemal@mail.ru>
2019-06-21 19:05:46 +0300

    Добавил события на сладйдера

--- src/main/java/controllers/PlayerController.java
+++ src/main/java/controllers/PlayerController.java
@@ -4,10 +4,13 @@ import client.PlayerClient;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+import start.Main;
 
 import java.io.File;
 import java.net.URL;
@@ -39,6 +42,7 @@ public class PlayerController implements Initializable {
     @FXML
     private ImageView volumeImage;
 
+    private boolean start = false;
 
     private File mediaFile = null;
 
@@ -49,6 +53,9 @@ public class PlayerController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         selectMediaFile();
+        setEventToPauseStart();
+        setActionsToSliderOfTheTrack();
+        setActionsToVolumeSlider();
     }
 
     @FXML
@@ -56,14 +63,104 @@ public class PlayerController implements Initializable {
         browseButton.setOnAction(event -> {
             // выбор медиафайла
             mediaFile = PlayerClient.chooseFile();
-
             // создаем плеер с даным файлом
-            if (mediaFile != null){
+            if (mediaFile != null) {
                 nameOfTheTrack.setText(mediaFile.getName());
                 media = new Media(mediaFile.toURI().toString());
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                }
                 mediaPlayer = new MediaPlayer(media);
+
+                // указание длинны слайдера трека
+                mediaPlayer.setOnReady(() -> {
+                    setParametersToSliderOfTheTrack();
+                });
+
+                // движение слайдера трека
+                mediaPlayer.setOnPlaying(() -> {
+                    Main.service.submit(() -> {
+                        while (start) {
+                            sliderOfTheTrack.setValue(mediaPlayer.getCurrentTime().toSeconds());
+                        }
+                    });
+                });
+
+                volumeSlider.setValue(mediaPlayer.getVolume() * 100);
                 mediaView.setMediaPlayer(mediaPlayer);
             }
+        });
+    }
+
+    // задание максимума по длинне трека в сикундах
+    private void setParametersToSliderOfTheTrack() {
+        sliderOfTheTrack.setMin(0);
+        sliderOfTheTrack.setMax(PlayerClient.getDurationOfTheFileS(media));
+        sliderOfTheTrack.setValue(0);
+    }
+
+    @FXML
+    private void setEventToPauseStart() {
+        play_pause.setOnMouseClicked(event -> {
+            startOrPause();
+        });
+    }
+
+    // пауза\запуск медиафайла
+    private void startOrPause() {
+        if (start) mediaPlayer.pause();
+        else mediaPlayer.play();
+        changeImagePause();
+        start = !start;
+    }
+
+    // смена картинки паузы
+    private void changeImagePause() {
+        if (start) {
+            File file = new File("C:\\Users\\Andrey\\IDEA\\MusicPlayer\\src\\main\\resources\\icons\\-play.png");
+            play_pause.setImage(new Image(file.toURI().toString()));
+        } else {
+            File file = new File("C:\\Users\\Andrey\\IDEA\\MusicPlayer\\src\\main\\resources\\icons\\pause.png");
+            play_pause.setImage(new Image(file.toURI().toString()));
+        }
+    }
+
+    //измененить время проигрывания слайдером
+    @FXML
+    private void setActionsToSliderOfTheTrack() {
+        sliderOfTheTrack.setOnMouseDragged(event -> {
+            eventToSliderOfTheTrack();
+        });
+
+        sliderOfTheTrack.setOnMouseClicked(event -> {
+            eventToSliderOfTheTrack();
+        });
+    }
+
+    //событие слайдера трека
+    private void eventToSliderOfTheTrack() {
+        start = false;
+        mediaPlayer.pause();
+        double value = sliderOfTheTrack.getValue();
+        Duration duration = Duration.seconds(value);
+        mediaPlayer.setStartTime(duration);
+        mediaPlayer.stop();
+        mediaPlayer.play();
+        start = true;
+    }
+
+
+    // изменение громкости на плеере
+    @FXML
+    private void setActionsToVolumeSlider() {
+        volumeSlider.setOnMouseDragged(event -> {
+            double value = volumeSlider.getValue();
+            mediaPlayer.setVolume(value / 100);
+        });
+
+        volumeSlider.setOnMouseClicked(event -> {
+            double value = volumeSlider.getValue();
+            mediaPlayer.setVolume(value / 100);
         });
     }
 }
