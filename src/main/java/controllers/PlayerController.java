3c9cf070a2e2132b23fd14096947ae7804573113
Dryunkaaa <the_quemal@mail.ru>
2019-06-21 16:18:42 +0300

    добавил загрузку файла

--- src/main/java/controllers/PlayerController.java
+++ src/main/java/controllers/PlayerController.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import client.PlayerClient;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import start.Main;
 
-public class PlayerController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerController implements Initializable {
 
     @FXML
     private ImageView play_pause;
@@ -31,5 +41,35 @@ public class PlayerController {
     @FXML
     private Label nameOfTheTrack;
 
+    private File mediaFile = null;
+
+    private Media media = null;
+
+    private MediaPlayer mediaPlayer = null;
+
+    @FXML
+    private void selectMediaFile() {
+        browseButton.setOnAction(event -> {
+            Main.service.submit(() -> {
+                mediaFile = PlayerClient.chooseFile();
+                Platform.runLater(() -> {
+                    nameOfTheTrack.setText(mediaFile.getName());
+                    media = new Media(mediaFile.toURI().toString());
+                    mediaPlayer = new MediaPlayer(media);
+                    mediaView.setMediaPlayer(mediaPlayer);
+                });
+            });
+        });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        selectMediaFile();
+        //Main.service.submit(() -> YouTubeAnalytic.rotatePicture(imageView));
+        //backToMain();
+        // backToYoutubeAnalytic();
+//        CompareInfo.show(searchButton, channelsIdField, tableView, nameColumn, dateColumn, subsColumn,
+//                videoColumn, viewsColumn, showTime);
+    }
 }
 
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,5 +1,6 @@
 package start;
 
+import controllers.PlayerController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,8 +22,11 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("/fxmls/player.fxml"));
-
+//        Parent root = FXMLLoader.load(getClass().getResource("/fxmls/player.fxml"));
+        PlayerController playerController = new PlayerController();
+        FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemClassLoader().getResource("fxmls/player.fxml"));
+        loader.setController(playerController);
+        Parent root = (Parent) loader.load();
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Player");
